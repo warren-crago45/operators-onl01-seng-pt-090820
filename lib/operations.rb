51a98da0a8517a110074f717edf5997ed1000d45
Warren Crago <warren.crago45@gmail.com>
2020-09-15 23:17:01 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
+  if speed < 40 || speed > 60
+    TRUE
+  else 
+    FALSE
+  end
 
 end
 
 
 
 def not_safe?(speed)
-	
+	if speed < 40 || speed > 60 ? TRUE : FALSE
+	  
 end
 	
 
